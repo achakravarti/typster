adebccba03cf63730ee726ab652d2b9e12954fe0
Abhishek Chakravarti <abhishek@taranjali.org>
2019-04-05 10:11:49 +0530

    Initialise video device
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/typster.c
+++ src/typster.c
@@ -1,4 +1,10 @@
 #include <SDL2/SDL.h>
+#include <stdio.h>
+
+
+        /* status codes */
+#define STATUS_OK 0
+#define STATUS_ERROR 1
 
 
         /* video device */
@@ -6,6 +12,50 @@ typedef struct __typster_video {
         SDL_Renderer *renderer;
         SDL_Window *window;
 } typster_video;
+
+
+        /* constructs a video device instance */
+int typster_video_ctor(typster_video *video)
+{
+        const int wflag = 0;
+        const int rflag = SDL_RENDERER_ACCELERATED;
+        const int width = 1280;
+        const int height = 720;
+
+                /* initialise SDL */
+        if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+                printf("Couldn't initialise SDL: %s\n", SDL_GetError());
+                return STATUS_ERROR;
+        }
+
+                /* create window */
+        video->window = SDL_CreateWindow(
+                "Typster",
+                SDL_WINDOWPOS_UNDEFINED,
+                SDL_WINDOWPOS_UNDEFINED,
+                height,
+                width,
+                wflag
+        );
+
+                /* handle window creation exception */
+        if (!video->window) {
+                printf("Failed to create window: %s\n", SDL_GetError());
+                return STATUS_ERROR;
+        }
+
+                /* create renderer */
+        SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+        video->renderer = SDL_CreateRenderer(video->window, -1, rflag);
+
+                /* handle renderer creation exception */
+        if (!video->renderer) {
+                printf("Failed to create renderer: %s\n", SDL_GetError());
+                return STATUS_ERROR;
+        }
+
+        return STATUS_OK;
+}
 
 
         /* handles the input for a frame */
@@ -28,13 +78,21 @@ void render(void)
 
 int main(int argc, char *argv[])
 {
-                /* run game loop */
+        auto typster_video video;
+
+                /* initialise video device */
+        if (typster_video_ctor(&video)) {
+                printf("Failed to initialise video device\n");
+                return STATUS_ERROR;
+        }
+
+                /* run game frame loop */
         while (1) {
                 input();
                 update();
                 render();
         }
 
-        return 0;
+        return STATUS_OK;
 }
 
