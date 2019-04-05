c7c0ef248fc12f6974a99c850ef199ce5a12ad98
Abhishek Chakravarti <abhishek@taranjali.org>
2019-04-05 09:38:29 +0530

    Add skeletal game loop
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/typster.c
+++ src/typster.c
@@ -1,7 +1,33 @@
 #include <SDL/SDL.h>
 
+
+        /* handles the input for a frame */
+void input(void)
+{
+}
+
+
+        /* updates the state of a frame */
+void update(void)
+{
+}
+
+
+        /* renders a frame */
+void render(void)
+{
+}
+
+
 int main(int argc, char *argv[])
 {
+                /* run game loop */
+        while (1) {
+                input();
+                update();
+                render();
+        }
+
         return 0;
 }
 
