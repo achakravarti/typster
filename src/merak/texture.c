510c653c470bcf932f0c75f980ced6ddee8ed342
Abhishek Chakravarti <abhishek@taranjali.org>
2019-04-10 09:02:37 +0530

    Simplify merak_texture_render()
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/merak/texture.c
+++ src/merak/texture.c
@@ -7,7 +7,7 @@
 
 struct __merak_texture {
         SDL_Texture *tex;
-        merak_area *area;
+        SDL_Rect rect;
 };
 
 
@@ -18,22 +18,24 @@ extern sol_erno merak_texture_new(merak_texture **tex, const char *fpath)
         auto merak_texture *ctx = SOL_PTR_NULL;
         auto SDL_Surface *surf = SOL_PTR_NULL;
         auto SDL_Renderer *brush = SOL_PTR_NULL;
-        auto int width, height;
 
 SOL_TRY:
         sol_assert (fpath && *fpath, SOL_ERNO_STR);
 
         sol_try (sol_ptr_new((sol_ptr **) tex, sizeof (**tex)));
         ctx = *tex;
-        ctx->tex = SOL_PTR_NULL;
-        ctx->area = SOL_PTR_NULL;
 
         surf = IMG_Load(fpath);
         sol_try (merak_screen_brush((sol_ptr **) &brush));
         ctx->tex = SDL_CreateTextureFromSurface(brush, surf);
+        sol_assert (ctx->tex, SOL_ERNO_STATE);
 
-        SDL_QueryTexture(ctx->tex, SOL_PTR_NULL, SOL_PTR_NULL, &width, &height);
-        sol_try (merak_area_new(&ctx->area, width, height));
+        ctx->rect.x = ctx->rect.y = 0;
+        SDL_QueryTexture(ctx->tex,
+                         SOL_PTR_NULL,
+                         SOL_PTR_NULL,
+                         &ctx->rect.w,
+                         &ctx->rect.h);
 
 SOL_CATCH:
         sol_log_erno(sol_erno_get());
@@ -55,14 +57,10 @@ extern void merak_texture_free(merak_texture **tex)
 {
         auto merak_texture *ctx;
 
-        if (sol_likely (tex && *tex)) {
-                ctx = *tex;
-
-                if (sol_likely (ctx)) {
+        if (sol_likely (tex && (ctx = *tex))) {
+                if (sol_likely (ctx->tex)) {
                         SDL_DestroyTexture(ctx->tex);
                 }
-
-                merak_area_free(&ctx->area);
         }
 
         sol_ptr_free((sol_ptr **) tex);
@@ -93,22 +91,19 @@ extern sol_erno merak_texture_render(const merak_texture *tex,
                                      const merak_point *loc)
 {
         auto SDL_Renderer *brush = SOL_PTR_NULL;
-        auto SDL_Rect src, dst;
+        //auto SDL_Rect src, dst;
+        auto SDL_Rect dst;
 
 SOL_TRY:
         sol_assert (tex, SOL_ERNO_PTR);
 
-        src.x = src.y = 0;
-        sol_try (merak_area_width(tex->area, (sol_uint *) &src.w));
-        sol_try (merak_area_height(tex->area, (sol_uint *) &src.h));
-
         sol_try (merak_point_x(loc, (sol_uint *) &dst.x));
         sol_try (merak_point_y(loc, (sol_uint *) &dst.y));
-        dst.w = src.w;
-        dst.h = src.h;
+        dst.w = tex->rect.w;
+        dst.h = tex->rect.h;
 
         sol_try (merak_screen_brush((sol_ptr **) &brush));
-        SDL_RenderCopy(brush, tex->tex, &src, &dst);
+        SDL_RenderCopy(brush, tex->tex, SOL_PTR_NULL, &dst);
 
 SOL_CATCH:
         sol_log_erno(sol_erno_get());
